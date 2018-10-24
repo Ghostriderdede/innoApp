25b118427463beef0a4c0730025deb956a29f842
Denise Nast <Denise.Nast@dvag.com>
2018-10-24 10:37:04 +0200

    Added first view

--- inno-App/src/app/app.component.ts
+++ inno-App/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  userInputID = '';
+  userInputPW = '';
+  user: User;
+
+  ngOnInit() {
+    const user = new User();
+    this.user = user;
+  }
+
+  userID(event: any) {
+    this.userInputID = event.target.value;
+    console.log(this.userInputID);
+    if (this.userID!== undefined) {
+      this.user.ID = this.userInputID;
+    }
+  }
+
+  userPW(event: any) {
+    this.userInputPW = event.target.value;
+    console.log(this.userInputPW);
+    if (this.userPW !== undefined) {
+      this.user.PW = this.userInputPW;
+    }
+  }
+
+  printUser() {
+    console.log(this.user);
+  }
 }
